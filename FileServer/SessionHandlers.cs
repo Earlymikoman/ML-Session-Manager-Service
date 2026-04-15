83ae5b9070a8b3cab234e2728e7bf0a3e92120e8
Earlymikoman <xboosinger@gmail.comgit config --global user.email xboosing>
2026-04-15 14:17:25 -0700

    minor tweak; removed caching behavior

--- FileServer/SessionHandlers.cs
+++ FileServer/SessionHandlers.cs
@@ -113,67 +113,67 @@ public class Sessions
         }
     }
 
-    public async Task LoginDelegate(HttpContext context)
-    {
-        using(var log = _logger.StartMethod(nameof(LoginDelegate), context))
-        {
-            try
-            {
-                HttpRequest request = context.Request;
+    // public async Task LoginDelegate(HttpContext context)
+    // {
+    //     using(var log = _logger.StartMethod(nameof(LoginDelegate), context))
+    //     {
+    //         try
+    //         {
+    //             HttpRequest request = context.Request;
 
-                UserMetadata m = new UserMetadata();
-                m.userid = GetParameterFromList("userid", request, log);
-                m.prompttype = GetParameterFromList("prompttype", request, log);
+    //             UserMetadata m = new UserMetadata();
+    //             m.userid = GetParameterFromList("userid", request, log);
+    //             m.prompttype = GetParameterFromList("prompttype", request, log);
 
-                log.SetAttribute("request.userid", m.userid);
-                log.SetAttribute("request.prompttype", m.prompttype);
+    //             log.SetAttribute("request.userid", m.userid);
+    //             log.SetAttribute("request.prompttype", m.prompttype);
 
-                // First step is we will write the metadata to CosmosDB
-                // Here we are using Type mapping to convert our data structure
-                // to a JSON document that can be stored in CosmosDB.
-                if (await _cosmosDbWrapper.GetItemAsync<UserMetadata>(m.id, m.userid) == null)
-                {
-                    await _cosmosDbWrapper.AddItemAsync(m, m.userid);
-                }
-
-
-
-                string responseString = "";
-
-                HttpResponse response = context.Response;
-
-                response.StatusCode = 200;
-                response.ContentLength = Encoding.UTF8.GetByteCount(responseString);
-                response.ContentType = "text/plain; charset=utf-8";
+    //             // First step is we will write the metadata to CosmosDB
+    //             // Here we are using Type mapping to convert our data structure
+    //             // to a JSON document that can be stored in CosmosDB.
+    //             if (await _cosmosDbWrapper.GetItemAsync<UserMetadata>(m.id, m.userid) == null)
+    //             {
+    //                 await _cosmosDbWrapper.AddItemAsync(m, m.userid);
+    //             }
 
 
 
-                var CurrentSessionData = new 
-                { 
-                    User = m.userid, 
-                    PromptType = m.prompttype, 
-                    PromptName = "None" 
-                };
-                string sessionJson = JsonSerializer.Serialize(CurrentSessionData);
+    //             string responseString = "";
 
-                //Grok knows its cookies
-                var cookieOptions = new CookieOptions
-                {
-                    Expires = DateTimeOffset.UtcNow.AddDays(1),   // or .AddHours(1), etc.
-                    HttpOnly = true,                              // Prevents JavaScript access (security)
-                    Secure = true,                                // Only send over HTTPS
-                    IsEssential = true,                           // For GDPR consent (if needed)
-                    SameSite = SameSiteMode.Strict                // or Lax / None
-                };
+    //             HttpResponse response = context.Response;
 
-                response.Cookies.Append("CurrentSessionData", sessionJson, cookieOptions);
-            }
-            catch(Exception e)
-            {
-                log.HandleException(e);
-            }
-        }
-    }
+    //             response.StatusCode = 200;
+    //             response.ContentLength = Encoding.UTF8.GetByteCount(responseString);
+    //             response.ContentType = "text/plain; charset=utf-8";
+
+
+
+    //             var CurrentSessionData = new 
+    //             { 
+    //                 User = m.userid, 
+    //                 PromptType = m.prompttype, 
+    //                 PromptName = "None" 
+    //             };
+    //             string sessionJson = JsonSerializer.Serialize(CurrentSessionData);
+
+    //             //Grok knows its cookies
+    //             var cookieOptions = new CookieOptions
+    //             {
+    //                 Expires = DateTimeOffset.UtcNow.AddDays(1),   // or .AddHours(1), etc.
+    //                 HttpOnly = true,                              // Prevents JavaScript access (security)
+    //                 Secure = true,                                // Only send over HTTPS
+    //                 IsEssential = true,                           // For GDPR consent (if needed)
+    //                 SameSite = SameSiteMode.Strict                // or Lax / None
+    //             };
+
+    //             response.Cookies.Append("CurrentSessionData", sessionJson, cookieOptions);
+    //         }
+    //         catch(Exception e)
+    //         {
+    //             log.HandleException(e);
+    //         }
+    //     }
+    // }
     public async Task GetSessionDataDelegate(HttpContext context)
     {
         using(var log = _logger.StartMethod(nameof(GetSessionDataDelegate), context))
@@ -215,7 +215,7 @@ public class Sessions
         }
     }
 
-    //Incomplete
+    //Complete
     public async Task WritePromptResponseDelegate(HttpContext context)
     {
         using(var log = _logger.StartMethod(nameof(WritePromptResponseDelegate), context))
@@ -355,27 +355,6 @@ public class Sessions
 
                     responseString = promptContent;
 
-
-
-                    var CurrentSessionData = new 
-                    { 
-                        User = m.userid, 
-                        PromptType = m.prompttype, 
-                        PromptName = promptToRequest 
-                    };
-                    string sessionJson = JsonSerializer.Serialize(CurrentSessionData);
-
-                    //Grok knows its cookies
-                    var cookieOptions = new CookieOptions
-                    {
-                        Expires = DateTimeOffset.UtcNow.AddDays(1),   // or .AddHours(1), etc.
-                        HttpOnly = true,                              // Prevents JavaScript access (security)
-                        Secure = true,                                // Only send over HTTPS
-                        IsEssential = true,                           // For GDPR consent (if needed)
-                        SameSite = SameSiteMode.Strict                // or Lax / None
-                    };
-
-                    response.Cookies.Append("CurrentSessionData", sessionJson, cookieOptions);
                     }
 
 
