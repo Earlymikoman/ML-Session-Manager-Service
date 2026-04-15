63c4cc62fd111b96abcbf88480aa89c92ee1d273
Earlymikoman <xboosinger@gmail.comgit config --global user.email xboosing>
2026-04-15 14:47:55 -0700

    need the app.MapGet for login

--- Program.cs
+++ Program.cs
@@ -60,7 +60,7 @@ class Program
         app.MapGet("/", instance.DefaultDelegate);
 
         app.MapGet("/healthcheck", instance.HealthCheckDelegate);
-        //app.MapGet("/login", instance.LoginDelegate);
+        app.MapGet("/login", instance.LoginDelegate);
         //app.MapGet("/getsessiondata", instance.GetSessionDataDelegate);
         app.MapGet("/acquireprompt", instance.AcquirePromptDelegate);
         app.MapPost("/writepromptresponse", instance.WritePromptResponseDelegate);
